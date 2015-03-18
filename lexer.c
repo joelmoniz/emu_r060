eed8a61035d8d2d669c7df5dc8b1e0a26fe32a52
Joel Moniz <jramoniz@gmail.com>
2015-03-18 14:27:52 +0530

    Adding header for symbol table

--- lexer.c
+++ lexer.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <symbol_table.h>
 
 //Need to do only one pass!
 //What was that joey blabbering about reading in blocks?
@@ -9,67 +10,6 @@
 // what about ! and != ??
 // @~ is a multi-line comment
     
-enum Token {
-   
-  tk_main, //main
-  tk_lbrace, //{
-  tk_rbrace, //}
-  tk_lpara, //(
-  tk_rpara, //)
-  tk_lsquare, //[
-  tk_rsquare,//]
-  tk_struct, // struct
-  tk_cmnt_begin, // @~
-  tk_cmnt_end, // ~@
-  tk_semi_cl,// ;
-  tk_assign_op, // =
-  tk_id, // [A­Z a­z] [A­Z a­z _ 0­9]* identifier
-  tk_comma, // , 
-  tk_num, //integer [1­9][0­9]* | [0] 
-  tk_rnum, // [0­9]+”.”[0­9]+ 
-  tk_bool, // boolean
-  tk_bot, //Bot
-  tk_velocity, //Velocity
-  tk_col_assign, // :=
-  tk_int, // int
-  tk_float, // float
-  tk_dot, // .
-  tk_colon, // :
-  tk_log_or, // ||
-  tk_log_and, // &&
-  tk_lt, // <
-  tk_gt, // > 
-  tk_log_eq, // ==
-  tk_lte, // <=
-  tk_gte, // >=
-  tk_func, // function
-  tk_return, // return
-  tk_rt, // rt
-  tk_addv, // addV
-  tk_pl_eq, // +=
-  tk_true, // true
-  tk_false, // false
-  tk_inpop, // >>
-  tk_unary_inc, // ++
-  tk_unary_dec, // --
-  tk_undersc, // _
-  tk_fw, // fw
-  tk_point, // Point
-  tk_void, //void
-  tk_readi, //readi
-  tk_break, //break
-  tk_continue,  //continue
-  tk_exit, //exit
-  tk_for, //for
-  tk_if, //if
-  tk_else, //else
-  tk_plus, //+
-  tk_minus, //-
-  tk_mul, //*
-  tk_div // /
-  
-};
-
 int isSymbol(char input)
 {
   if(input==' ' || input=='+' || input=='-' || input=='*' || input=='/' || input=='[' || input==']' ||

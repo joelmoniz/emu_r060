a98ed297a7d9a7b790d42ceb9b827fd3f9a7d233
Joel Moniz <jramoniz@gmail.com>
2015-04-20 00:34:34 +0530

    Adding one more (good) test case

--- generate_output.sh
+++ generate_output.sh
@@ -3,8 +3,10 @@
 ./a.out ./test_cases/submitted_test3.txt > ./test_cases/output3.txt
 ./a.out ./test_cases/submitted_test4.txt > ./test_cases/output4.txt
 ./a.out ./test_cases/submitted_test5.txt > ./test_cases/output5.txt
+./a.out ./test_cases/misc_test1.txt > ./test_cases/output1m.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output1.txt > ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output2.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output3.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output4.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output5.txt >> ./test_cases/out_compare.txt
+sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output1m.txt > ./test_cases/out_compare_misc.txt

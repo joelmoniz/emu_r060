08c6f43937beb56bfbac9eb53947063bc3b62c06
Joel Moniz <jramoniz@gmail.com>
2015-04-20 12:29:49 +0530

    New test case

--- generate_output.sh
+++ generate_output.sh
@@ -4,9 +4,11 @@
 ./a.out ./test_cases/submitted_test4.txt > ./test_cases/output4.txt
 ./a.out ./test_cases/submitted_test5.txt > ./test_cases/output5.txt
 ./a.out ./test_cases/misc_test1.txt > ./test_cases/output1m.txt
+./a.out ./test_cases/misc_test2.txt > ./test_cases/output2m.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output1.txt > ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output2.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output3.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output4.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output5.txt >> ./test_cases/out_compare.txt
 sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output1m.txt > ./test_cases/out_compare_misc.txt
+sed -n -e '/Abstract Syntax tree:/,$p' ./test_cases/output2m.txt >> ./test_cases/out_compare_misc.txt
